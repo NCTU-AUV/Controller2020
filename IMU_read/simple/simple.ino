51e034790529831ce10445fa3390ae36ee604a45
Fishbone Huang <eric56421@gmail.com>
2020-12-02 23:09:25 +0800

    Using glob to get the arduino port and the arduino code.

--- IMU_read/simple/simple.ino
+++ IMU_read/simple/simple.ino
@@ -1,5 +1,7 @@
-// https://github.com/hideakitai/MPU9250
-
+/*
+/ MPU9250 library: https://github.com/hideakitai/MPU9250
+/ Magnetic Declination: https://www.magnetic-declination.com/
+*/
 
 #include "MPU9250.h"
 
@@ -21,7 +23,8 @@ void setup()
     Wire.begin();
 
     delay(2000);
-    mpu.setup();
+    mpu.setMagneticDeclination(-6.33);
+    mpu.setup(0x68);
 }
 
 void loop()
@@ -32,21 +35,25 @@ void loop()
         mpu.update();
         // mpu.print();
         
-        // Direct use write() to sedn buffer data
+        // Direct use write() to send buffer data
+        /*
         Imu data = {mpu.getAcc(0), mpu.getAcc(1), mpu.getAcc(2),
                     mpu.getGyro(0), mpu.getGyro(1), mpu.getGyro(2),
                     mpu.getMag(0), mpu.getMag(1), mpu.getMag(2),
                     mpu.getRoll(), mpu.getYaw(), mpu.getPitch()};
         Serial.write((byte*)&data, sizeof(data));
         Serial.write(10);
-
-        // Could use print() & println() to sedn over String
-        /*
-        Serial.print(mpu.getAcc(0));
-        Serial.print(" ");
-        Serial.print(mpu.getAcc(1));
-        Serial.println();
         */
+
+        // Could use print() & println() to send over String
+        Serial.print(mpu.getRoll());
+        Serial.print(" ");
+        Serial.print(mpu.getPitch());
+        Serial.print(" ");
+        Serial.print(mpu.getYaw());
+        Serial.print(" ");
+        Serial.print(mpu.getTemperature());
+        Serial.println();
 
         prev_ms = millis();
     }
