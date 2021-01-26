d7d6989f629542a62e0b7848c4c4d6f3c820f9f9
zzhh956 <ok10155096@gmail.com>
2021-01-26 18:23:31 +0800

    update for IMU visualization

--- control_pkg/script/imu_9250.py
+++ control_pkg/script/imu_9250.py
@@ -6,48 +6,42 @@ import rospy
 import threading
 from std_msgs.msg import Float64MultiArray
 from std_msgs.msg import Float64
+from sensor_msgs.msg import Imu
 import time
 import random
+from tf.transformations import euler_from_quaternion
 
 class IMUAttitude:
     def __init__(self):
         self.arduino_port = glob.glob('/dev/ttyACM*')[0]       
         
-        self.arduino = serial.Serial(self.arduino_port, 9600, timeout=1)
+        self.arduino = serial.Serial(self.arduino_port, 115200, timeout=1)
         if not self.arduino.is_open:
             self.arduino.open()
 
         rospy.on_shutdown(self.shutdown)
         rospy.init_node('IMU_9250', anonymous=True)
-        self.arr_pub = rospy.Publisher('IMU/Attitude', Float64MultiArray, queue_size=10)
-        
-        #new
-        self.roll_pub = rospy.Publisher('IMU/Roll', Float64, queue_size=10)
-        self.pitch_pub = rospy.Publisher('IMU/Pitch', Float64, queue_size=10)
-        self.yaw_pub = rospy.Publisher('IMU/Yaw', Float64, queue_size=10)
-        self.temp_pub = rospy.Publisher('IMU/Temperature', Float64, queue_size=10)
-        
+
+        #For PID
+        self.arr_pub = rospy.Publisher('IMU/Attitude', Float64MultiArray, queue_size=10) #[roll, pitch, yaw]
+
+        #For visualize
+        self.imu_visualize_data = Imu()
+        self.imu_visualize_pub = rospy.Publisher('IMU/Visualize', Imu, queue_size=100)
+
         self.imu_t = threading.Thread(target=self.get_data, daemon=True)
         self.imu_t.start()
 
         rospy.spin()
     
     def get_data(self):
-        attitude = [0.0]*4
         while self.arduino.is_open:
             try:
                 raw_data = self.arduino.readline()
                 #print('arduino raw data: ')
                 #print(raw_data)
-                attitude = [float(val) for val in raw_data.split()]
-                if len(attitude) != 4:
-                    print('Error: len != 4')
-                    continue
-
-                roll = float(attitude[0])
-                pitch = float(attitude[1])
-                yaw = float(attitude[2])
-                temperature = float(attitude[3])
+                data = unpack('ffffffffffc', raw_data) 
+                attitude = euler_from_quaternion(float(data[0:4]))
 
             except Exception as e:
                 print('oops')
@@ -55,12 +49,26 @@ class IMUAttitude:
                 # time.sleep(0.5)
                 #print(attitude)
             
+            #roll, pitch, yaw
             rospy.loginfo(attitude)
             self.arr_pub.publish(Float64MultiArray(data=attitude))
-            self.roll_pub.publish(Float64(data=roll))
-            self.pitch_pub.publish(Float64(data=pitch))
-            self.yaw_pub.publish(Float64(data=yaw))
-            self.temp_pub.publish(Float64(data=temperature))
+            
+            #imu.orientation
+            self.imu_visualize_data.orientation = [float(data[0:4])]
+            self.imu_visualize_data.orientation_covariance[0] = -1
+
+            #imu.angular_velocity
+            self.imu_visualize_data.angular_velocity = [float(data[4:7])]
+            self.imu_visualize_data.angular_velocity_covariance[0] = -1
+            
+            #imu.linear_acceleration
+            self.imu_visualize_data.linear_acceleration = [float(data[7:10])]
+            self.imu_visualize_data.linear_acceleration_covariance[0] = -1
+
+            self.imu_visualize_pub.publish(self.imu_visualize_data)
+
+            rospy.sleep(0.01) #100 hz
+
 
     def shutdown(self):
         self.arduino.close()
