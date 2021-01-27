c83e02ac7eeadd5c1c4bf111dc663e3a7b284f87
Fishbone Huang <eric56421@gmail.com>
2021-01-27 10:48:48 +0800

    imu visualize

--- control_pkg/script/imu_9250.py
+++ control_pkg/script/imu_9250.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 
 import serial
 import glob
@@ -9,6 +9,8 @@ from std_msgs.msg import Float64
 from sensor_msgs.msg import Imu
 import time
 import random
+from struct import unpack
+#import tf.transformations
 from tf.transformations import euler_from_quaternion
 
 class IMUAttitude:
@@ -16,8 +18,10 @@ class IMUAttitude:
         self.arduino_port = glob.glob('/dev/ttyACM*')[0]       
         
         self.arduino = serial.Serial(self.arduino_port, 115200, timeout=1)
-        if not self.arduino.is_open:
+        
+        while not self.arduino.is_open:
             self.arduino.open()
+            print("fail to open the arduino")
 
         rospy.on_shutdown(self.shutdown)
         rospy.init_node('IMU_9250', anonymous=True)
@@ -27,48 +31,63 @@ class IMUAttitude:
 
         #For visualize
         self.imu_visualize_data = Imu()
-        self.imu_visualize_pub = rospy.Publisher('IMU/Visualize', Imu, queue_size=100)
+        self.imu_visualize_data.header.frame_id = "map"
+        self.imu_visualize_pub = rospy.Publisher('IMU/Visualize', Imu, queue_size=10)
 
-        self.imu_t = threading.Thread(target=self.get_data, daemon=True)
+        self.imu_t = threading.Thread(target=self.get_data)
+        #self.imu_t = threading.Thread(target=self.get_data, daemon=True)
         self.imu_t.start()
+
+        self.rate = rospy.Rate(100)
 
         rospy.spin()
     
     def get_data(self):
+        data = [0.0] * 11
+
         while self.arduino.is_open:
             try:
                 raw_data = self.arduino.readline()
                 #print('arduino raw data: ')
                 #print(raw_data)
                 data = unpack('ffffffffffc', raw_data) 
-                attitude = euler_from_quaternion(float(data[0:4]))
-
+               
             except Exception as e:
                 print('oops')
                 print(e)
                 # time.sleep(0.5)
                 #print(attitude)
             
+            #attitude = euler_from_quaternion(data[0:4])
+            
             #roll, pitch, yaw
-            rospy.loginfo(attitude)
-            self.arr_pub.publish(Float64MultiArray(data=attitude))
+            #rospy.loginfo(attitude)
+            #self.arr_pub.publish(Float64MultiArray(data=attitude))
             
             #imu.orientation
-            self.imu_visualize_data.orientation = [float(data[0:4])]
-            self.imu_visualize_data.orientation_covariance[0] = -1
+            self.imu_visualize_data.orientation.w = data[0]
+            self.imu_visualize_data.orientation.x = data[1]
+            self.imu_visualize_data.orientation.y = data[2]
+            self.imu_visualize_data.orientation.z = data[3]
+            self.imu_visualize_data.orientation_covariance[0] = -1.0
 
             #imu.angular_velocity
-            self.imu_visualize_data.angular_velocity = [float(data[4:7])]
-            self.imu_visualize_data.angular_velocity_covariance[0] = -1
+            self.imu_visualize_data.angular_velocity.x = data[4]
+            self.imu_visualize_data.angular_velocity.y = data[5]
+            self.imu_visualize_data.angular_velocity.z = data[6]
+            self.imu_visualize_data.angular_velocity_covariance[0] = -1.0
             
             #imu.linear_acceleration
-            self.imu_visualize_data.linear_acceleration = [float(data[7:10])]
-            self.imu_visualize_data.linear_acceleration_covariance[0] = -1
-
+            self.imu_visualize_data.linear_acceleration.x = data[7]
+            self.imu_visualize_data.linear_acceleration.y = data[8]
+            self.imu_visualize_data.linear_acceleration.z = data[9]
+            self.imu_visualize_data.linear_acceleration_covariance[0] = -1.0
+                
             self.imu_visualize_pub.publish(self.imu_visualize_data)
+            print(data)
+            #print(type(self.imu_visualize_data))
 
-            rospy.sleep(0.01) #100 hz
-
+            #self.rate.sleep() #100 hz
 
     def shutdown(self):
         self.arduino.close()
